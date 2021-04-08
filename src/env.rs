13d8dcfd1268c94d0e969f3b7afa9893f5e44151
Ussama Azam <nuclearoreo@gmail.com>
2021-04-07 22:59:25 -0400

    Set up binding usage so having a variable map to a value in the enviroment

--- src/env.rs
+++ src/env.rs
@@ -1,7 +1,7 @@
 use crate::val::Val;
 use std::collections::HashMap;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Default)]
 pub(crate) struct Env {
     bindings: HashMap<String, Val>,
 }
@@ -9,5 +9,12 @@ pub(crate) struct Env {
 impl Env {
     pub(crate) fn store_binding(&mut self, name: String, val: Val) {
         self.bindings.insert(name, val);
+    }
+
+    pub(crate) fn get_binding_value(&self, name: &str) -> Result<Val, String> {
+        self.bindings
+            .get(name)
+            .cloned()
+            .ok_or_else(|| format!("binding with name ‘{}’ does not exist", name))
     }
 }
