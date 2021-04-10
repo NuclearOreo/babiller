6d144f64e92e1e7c49199784082e73d7bd884faa
Ussama Azam <nuclearoreo@gmail.com>
2021-04-09 23:14:55 -0400

    Adding tests to parse through block

--- src/block.rs
+++ src/block.rs
@@ -8,18 +8,47 @@ pub struct Block {
 
 impl Block {
     pub fn new(s: &str) -> Result<(&str, Self), String> {
-        let s = utils::tag("{}", s)?;
+        let s = utils::tag("{", s)?;
+        let (s, _) = utils::extract_whitespace(s);
 
-        Ok((s, Self { stmts: Vec::new() }))
+        let (s, stmts) = if let Ok((s, stmt)) = Stmt::new(s) {
+            (s, vec![stmt])
+        } else {
+            (s, Vec::new())
+        };
+
+        let (s, _) = utils::extract_whitespace(s);
+        let s = utils::tag("}", s)?;
+
+        Ok((s, Self { stmts }))
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::expr::{Expr, Number};
 
     #[test]
     fn parse_empty_block() {
         assert_eq!(Block::new("{}"), Ok(("", Block { stmts: Vec::new() })));
+    }
+
+    #[test]
+    fn parse_empty_block_with_space() {
+        assert_eq!(Block::new("{     }"), Ok(("", Block { stmts: Vec::new() })));
+    }
+
+    #[test]
+    fn parse_block_with_one_stmt() {
+        assert_eq!(
+            Block::new("{ 5 }"),
+            Ok((
+                "",
+                Block {
+                    stmts: vec![Stmt::Expr(Expr::Number(Number(5)))]
+                }
+            ))
+        );
     }
 }
