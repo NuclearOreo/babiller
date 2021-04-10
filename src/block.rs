682ad2bb0d1d5fc3280ab6021db3f3f0814930ea
Ussama Azam <nuclearoreo@gmail.com>
2021-04-09 23:03:41 -0400

    Implemented the begining of block

--- src/block.rs
+++ src/block.rs
@@ -1,16 +1,25 @@
-use crate::expr::Expr;
+use crate::stmt::Stmt;
+use crate::utils;
 
 #[derive(Debug, PartialEq)]
 pub struct Block {
-    pub exprs: Vec<Expr>,
+    pub stmts: Vec<Stmt>,
+}
+
+impl Block {
+    pub fn new(s: &str) -> Result<(&str, Self), String> {
+        let s = utils::tag("{}", s)?;
+
+        Ok((s, Self { stmts: Vec::new() }))
+    }
 }
 
 #[cfg(test)]
-mod test {
+mod tests {
     use super::*;
 
     #[test]
     fn parse_empty_block() {
-        assert_eq!(Block::new("{}"), Ok(("", Block { expr: Vec::new() })));
+        assert_eq!(Block::new("{}"), Ok(("", Block { stmts: Vec::new() })));
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ pub mod binding_def;
 pub mod binding_usage;
 pub mod block;
 pub mod expr;
+pub mod stmt;
 pub mod val;
 
 mod env;
