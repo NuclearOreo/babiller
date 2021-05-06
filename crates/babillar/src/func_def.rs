e3251d1abbc400a8fa4c5433af37c7cbe4825441
Ussama Azam <nuclearoreo@gmail.com>
2021-05-06 18:34:17 -0400

    Added ability to evaluate variables in expresions

--- crates/babillar/src/func_def.rs
+++ crates/babillar/src/func_def.rs
@@ -40,7 +40,7 @@ impl FuncDef {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::expr::{Block, Expr};
+    use crate::expr::{BindingUsage, Block, Expr, Op};
 
     #[test]
     fn parse_func_def_with_no_params_and_empty_body() {
@@ -67,6 +67,30 @@ mod tests {
                     name: "greet".to_string(),
                     params: vec!["name".to_string()],
                     body: Box::new(Stmt::Expr(Expr::Block(Block { stmts: Vec::new() }))),
+                },
+            )),
+        );
+    }
+
+    #[test]
+    #[test]
+    fn parse_func_def_with_multiple_params() {
+        assert_eq!(
+            FuncDef::new("fn add x y => x + y"),
+            Ok((
+                "",
+                FuncDef {
+                    name: "add".to_string(),
+                    params: vec!["x".to_string(), "y".to_string()],
+                    body: Box::new(Stmt::Expr(Expr::Operation {
+                        lhs: Box::new(Expr::BindingUsage(BindingUsage {
+                            name: "x".to_string(),
+                        })),
+                        rhs: Box::new(Expr::BindingUsage(BindingUsage {
+                            name: "y".to_string(),
+                        })),
+                        op: Op::Add,
+                    })),
                 },
             )),
         );
