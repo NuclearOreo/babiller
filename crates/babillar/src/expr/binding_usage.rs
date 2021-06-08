51983a8fab4195e2194788bf0c6c07eb513ed5ae
Ussama Azam <nuclearoreo@gmail.com>
2021-06-07 23:32:11 -0400

    Added errors possible for function evaluation and able to evaluate functions with no params

--- crates/babillar/src/expr/binding_usage.rs
+++ crates/babillar/src/expr/binding_usage.rs
@@ -1,4 +1,5 @@
 use crate::env::Env;
+use crate::expr::FuncCall;
 use crate::utils;
 use crate::val::Val;
 
@@ -20,7 +21,17 @@ impl BindingUsage {
     }
 
     pub(super) fn eval(&self, env: &Env) -> Result<Val, String> {
-        env.get_binding(&self.name)
+        env.get_binding(&self.name).or_else(|error_msg| {
+            if env.get_func(&self.name).is_ok() {
+                FuncCall {
+                    callee: self.name.clone(),
+                    params: Vec::new(),
+                }
+                .eval(env)
+            } else {
+                Err(error_msg)
+            }
+        })
     }
 }
 
